1ae4789af88e9526fdcfe4f860fc609225e25e1b
Hafiz Hamza <hafiz.hamza@brainplow.com>
2020-02-10 15:29:47 +0500

    push

--- app/src/main/java/com/bilalandcompany/issbpreparation/MainActivity.kt
+++ app/src/main/java/com/bilalandcompany/issbpreparation/MainActivity.kt
@@ -2,11 +2,16 @@ package com.bilalandcompany.issbpreparation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bilalandcompany.issbpreparation.kotlin.ActivityNavigator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigate?.setOnClickListener {
+            ActivityNavigator<PostDataActivity>(this,PostDataActivity::class.java)
+        }
     }
 }
